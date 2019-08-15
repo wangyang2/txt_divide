0563bab60769c3a8f04901cf511ea015cb9534c6
wangyang2 <49228515+wangyang2@users.noreply.github.com>
2019-08-15 14:45:20 +0800

    Create divide.py

---
+++ divide.py
@@ -0,0 +1,12 @@
+# -*- coding:UTF-8 -*-
+filecount = 0
+
+file1 = open('wenben.txt', 'r', encoding='utf-8-sig')
+fw = open(r'C:\Users\viruser.v-desktop\Desktop\fdc_wenben\fdc_txt_result'+str(filecount)+'.txt', 'w+', encoding='utf-8-sig')
+for i in file1:
+    print(len(i))
+    if len(i) == 1:
+        filecount += 1
+        fw = open(r'C:\Users\viruser.v-desktop\Desktop\fdc_wenben\fdc_txt_result'+str(filecount)+'.txt', 'w+', encoding='utf-8-sig')
+    else:
+        fw.write(i)
